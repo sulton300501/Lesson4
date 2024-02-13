095604efa2353ef8aebfb8ec0414b257547c0d66
sulton <global-sultonmalik326@gmail.com>
2024-02-13 14:47:28 +0500

    Sulton

---
+++ Product_API/Program.cs
@@ -0,0 +1,23 @@
+using Product_API.Repositories;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ Product_API/Repositories/IProductPostgressRepository.cs
@@ -0,0 +1,17 @@
+﻿﻿using Product_API.Models;
+
+namespace Product_API.Repositories
+{
+    public class ProductPostgressRepository : IProductRepository
+    {
+        public Product Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
